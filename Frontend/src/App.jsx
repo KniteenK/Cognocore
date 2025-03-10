41f1f3c211f173bf57e0f4e9907c35ca656fed83
Niteen Kumar <niteen22336@iiitd.ac.in>
2025-03-11 02:44:35 +0530

    fix: backend url fixed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const apiUrl = 'https://cognocore.onrender.com';
 
 
 function App() {
